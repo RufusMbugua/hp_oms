e5d6514f742bad6ec0c695ad8856ad79b0905a36
MwangiKevin <mwangikevinn@gmail.com>
2014-09-17 11:39:16 +0300

    added email functionality
    Signed-off-by: MwangiKevin <mwangikevinn@gmail.com>

--- application/modules/alerts/controllers/email.php
+++ application/modules/alerts/controllers/email.php
@@ -3,7 +3,78 @@ if (!defined('BASEPATH'))exit('No direct script access allowed');
 
 class email extends MY_Controller {
 	
+
+	//Mail server credentials	
+	public $imap_server;
+	public $imap_user;
+	public $imap_pass;
+	public $smtp_config;
+
+
 	public function __construct(){
+
+		$this->load->config("email");
+
+		$this->imap_server 		=		$this->config->item("imap_server");
+		$this->imap_user 		=		$this->config->item("imap_user");
+		$this->imap_pass 		=		$this->config->item("imap_pass");
+		$this->smtp_config 		=		$this->config->item("smtp_config");
+
+		$this->load->model('email_model');
+		$this->load->library('Imap');
+	}
+
+
+	/*
+	|
+	|--------------------------------------------------------------------------
+	|Send email
+	|--------------------------------------------------------------------------
+		|
+		|	@var receipients
+		|
+		|		$receipients is a string of email addresses separated by comma
+		|		@example
+		|
+		|			"mwangikevinn@gmail.com,rufusmbugua@gmail.com"
+		|
+		|
+		|	@var attachments
+		|
+		|		$attachments is an array bearing paths to files that are to be attached	|	
+		|		@example
+		|
+		|			array(
+		|					"../file1.jpg",
+		|					"../file2.jpg"
+		|				)
+	|	
+	*/
+	public function send($receipients,$subject=Null,$message=Null,$attachments=array(),$sender_description="HP OMS",$mailtype="html"){
+
+		$time	=	date('Y-m-d');
+		
+		$this->load->library('email', $this->smtp_config);
+
+		$this->email->set_newline("\r\n");
+		$this->email->from($this->imap_user, $sender_description);
+		$this->email->to($receipients);
+		$this->email->subject($subject);
+		$this->email->message($message);
+		$this->email->set_mailtype($mailtype);
+
+		if(sizeof($attachments)>0){
+			foreach ($attachments as $key => $file) {
+				$this->email->attach($file);
+			}
+		}
+		
+		if($this->email->send()){
+			$this->email_model->log($receipients, $subject, $message);
+		}else{
+			show_error($this->email->print_debugger());
+		}
+		
 
 	}
 }
--- application/modules/alerts/models/email_model.php
+++ application/modules/alerts/models/email_model.php
@@ -4,7 +4,21 @@ if (!defined('BASEPATH'))
 
 class email_model extends MY_Model{
 	
-	function __construct()	{
+	function __construct(){
 		
+	}
+
+	public function log($receipients, $subject, $message){
+
+		$emails = array(
+			'el_id' 		   	=>   NULL,
+			'el_subject'      	=>   $subject,
+			'el_message'      	=>   $message,
+			'el_recipients'   	=>   $receipients,
+			'el_timestamp'   	=>   NULL
+			);
+
+		$insert = $this->db->insert('email_log', $emails);
+
 	}
 }
