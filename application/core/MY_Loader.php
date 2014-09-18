8cbab21e783f9464411cde8e8deb080652ea00af
MwangiKevin <mwangikevinn@gmail.com>
2014-09-19 01:59:33 +0300

    template loader function:
    use:
    
    $this->load->template($data);
    
    Signed-off-by: MwangiKevin <mwangikevinn@gmail.com>

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -3,4 +3,20 @@
 /* load the MX_Loader class */
 require APPPATH."third_party/MX/Loader.php";
 
-class MY_Loader extends MX_Loader {}
+class MY_Loader extends MX_Loader {
+
+	function __construct() {
+		parent::__construct();
+	}
+
+	public function template($data =null) {
+
+		if(!is_null($data)){
+			$this->load->module('template');
+			$this->template->index($data);
+		}else{			
+			show_error('No $data parsed to template');
+		}
+
+	}
+}
